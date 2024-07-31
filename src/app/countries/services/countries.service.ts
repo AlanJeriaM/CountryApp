482224da7e367faaf036cee6eb0f7a03a77caa63
Alan Jeria Medina <alanjeria01@gmail.com>
2024-07-31 12:18:39 -0400

    refactorizacion countries.service.ts

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, count, map, of} from 'rxjs';
+import { Observable, catchError, count, delay, map, of} from 'rxjs';
 import { Country } from '../interfaces/country';
 
 
@@ -11,9 +11,31 @@ export class CountriesService {
 
   private apiUrl:string= 'https://restcountries.com/v3.1';
 
-
-
   constructor(private http: HttpClient) { }
+
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>( url )
+    .pipe(
+      catchError(error => of([])),
+      delay(1000),
+    );
+  }
+
+  searchCapital(term: string): Observable<Country[]>{
+    const url = `${this.apiUrl}/capital/${term}`
+    return this.getCountriesRequest(url)
+  }
+
+  searchCountry(term: string): Observable<Country[]>{
+    const url = `${this.apiUrl}/name/${term}`
+    return this.getCountriesRequest(url)
+  }
+
+
+  searchRegion(term: string): Observable<Country[]>{
+  const url = `${this.apiUrl}/region/${term}`
+    return this.getCountriesRequest(url)
+  }
 
 
   searchCountryByAlphaCode(term: string): Observable<Country | null>{
@@ -21,31 +43,6 @@ export class CountriesService {
     .pipe(
       map(countries => countries.length > 0 ? countries[0]: null),
       catchError(error => of(null))
-    );
-
-  }
-
-
-  searchCapital(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
-    .pipe(
-      catchError(error => of([]))
-    );
-
-  }
-
-  searchCountry(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${term}`)
-    .pipe(
-      catchError(error => of([]))
-    );
-
-  }
-
-  searchRegion(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/region/${term}`)
-    .pipe(
-      catchError(error => of([]))
     );
 
   }
